9ab38b482381e9f9fefdae5a82e6eaad253ee224
Zachary Weeden <zdw7287@rit.edu>
2016-12-25 00:23:03 -0500

    Initial commit

---
+++ snkrs.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+import json, time, datetime, requests
+session=requests.session()
+items={}
+url='https://api.nike.com/commerce/productfeed/products/snkrs/threads?country=US&limit=20&locale=en_US&withCards=true'
+while 1:
+	r=session.get(url)
+	itemsCaptured=0
+	previousCapture=0
+	for card in r.json()['threads']:
+		try:
+			cardAttribs=[]
+			title=str(card['product']['title'])
+			launchDate=str(card['product']['estimatedLaunchDate'])
+			imageUrl=str(card['product']['imageUrl'])
+			styleCode=str(card['product']['style']+'-'+card['product']['colorCode'])
+			cardAttribs.append(title)
+			cardAttribs.append(launchDate)
+			cardAttribs.append(imageUrl)
+			if styleCode not in items.keys():
+				items[styleCode]=cardAttribs
+				print('')
+				print(str(datetime.datetime.now()) + ' :: '+ styleCode+' FOUND!')
+				print(items[styleCode])
+				print('')
+				itemsCaptured+=1
+			else:
+				pass
+		except:
+			continue
+	if previousCapture==itemsCaptured:
+		print(str(datetime.datetime.now()) + ' :: '+'No new items found this pass!')
+	else:
+		#New items found/deleted! Should be outputted via line 21
+		previousCapture=itemsCaptured
+	time.sleep(60)
